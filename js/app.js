0e696fba8fda32502e4ea043aedf4a87db57b007
Alex Kyungho Park <kyungho96@gmail.com>
2017-07-09 17:55:06 +0900

    final

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
 $(document).ready(function(){
 // translate: when the document is fully loaded, launch the function following
-	var $heart = $(".heart"),
-		$addComment = $(".photo__addcomment-area");
+	var $heart = $('.heart'),
+		$addComment = $(".photo__addcomment-area")
+		$ellipsis = $('.fa-ellipsis-h'),
+        $popUp = $('.popUp'),
+        $closePopup = $('.fa-times'),
+        $closePopUpBtn = $('.closePopUpBtn');
 // function: declare the variable, and allocate all items with .heart class
 
 	$heart.click(function(){
+		var likes = $(this).parent().parent()
+					.children('.photo__likes')
+					.children('.photo__likes-number'),
+			likesNumber = parseInt(likes.html()),
+			newValue
 		if($(this).hasClass('fa-heart-o')){
-			console.log("+1")
+			newValue = likesNumber + 1;
 		} else{
-			console.log("-1")
+			newValue = likesNumber - 1;
 		}
+		likes.html(newValue)
 		$(this).toggleClass("fa-heart-o fa-heart")
 	});
 
@@ -24,7 +34,22 @@ $(document).ready(function(){
 			$(this).val('').blur();
 			//Append a <li> to comments//
 			commentList.append("<li class='photo__comment'>\
-						<span class='photo__comment--author'>Serranoarevalo</span> " + newComment + "</li>");
+						<span class='photo__comment--author'>b_dongjun</span> " + newComment + "</li>");
 		}
 	})
+
+ 	$ellipsis.click(function(){
+        //show popup
+        $popUp.fadeIn();
+    })
+
+    $closePopup.click(closePopup);
+    $closePopUpBtn.click(closePopup);
+
+    function closePopup(event){
+        event.preventDefault()
+        $popUp.fadeOut();
+    }
+
+
 })
